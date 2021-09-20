4ed4f2f32786b849e217d29057471de84b924ec6
micouy <szpontaniczny@gmail.com>
2021-09-20 11:54:55 +0200

    Format code

--- benches/benches.rs
+++ benches/benches.rs
@@ -2,12 +2,16 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use powierza_distance::powierza_distance;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("bench 1", |b| b.iter(|| {
-		powierza_distance(
-			black_box("abc_jkl_mno_xyz"),
-			black_box("xyz_mno_jkl_abc_mno_jkl_xyz_abc_mon_jkl_mno_xyz_xyz")
-		)
-	}));
+    c.bench_function("bench 1", |b| {
+        b.iter(|| {
+            powierza_distance(
+                black_box("abc_jkl_mno_xyz"),
+                black_box(
+                    "xyz_mno_jkl_abc_mno_jkl_xyz_abc_mon_jkl_mno_xyz_xyz",
+                ),
+            )
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
