a40d201a444ca90ab231ae17b6b7442726e1117b
ciele-club <ciele@protonmail.com>
2022-12-27 21:21:08 +0100

    Fix bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,71 @@
 #![doc = include_str!("../README.md")]
 
+/// Computes Powierża coefficient of a pattern and a piece of text. For a
+/// detailed explanation, see [lib's docs](crate).
+pub fn powierża_coefficient(pattern: &str, text: &str) -> Option<u32> {
+    let text_len = text.chars().count();
+    let pattern_len = pattern.chars().count();
+
+    // The coefficient is undefined if either of the three is true:
+    // 1. The pattern is longer than the text.
+    // 2. The pattern length is 0.
+    // 3. The text length is 0. (This check is unnecessary but makes the
+    //    code more understandable.)
+    if pattern_len > text_len || pattern_len == 0 || text_len == 0 {
+        return None;
+    }
+
+    // Pad the matrix with 0s on the top.
+    let mut cache = vec![Some(0); text_len];
+
+    let mut left = None;
+    let mut is_left_continuation = false;
+    let mut first_match_in_row_ix = None;
+
+    for pattern_char in pattern.chars() {
+        left = None;
+
+        // Pad the matrix with 0s on the left.
+        let mut diagonal = Some(0);
+
+        let text_chars = text.chars().enumerate().skip(first_match_in_row_ix.unwrap_or(0));
+
+        for (x, text_char) in text_chars {
+            let is_match = pattern_char == text_char;
+
+            let (current_score, is_continuation) =
+                calc_score(is_match, diagonal, left, is_left_continuation);
+
+            if is_continuation {
+                first_match_in_row_ix = first_match_in_row_ix.or(Some(x))
+            }
+
+            if x > 0 {
+                cache[x - 1] = left;
+            }
+
+            left = current_score;
+            is_left_continuation = is_continuation;
+            diagonal = cache[x];
+        }
+
+        // Terminate if no match was found in the current row.
+        if first_match_in_row_ix.is_none() {
+            return None;
+        }
+    }
+
+    // Fill the score of the right-most cell in the last row.
+    cache[text_len - 1] = left;
+
+    // The lowest score in the last row is the coefficient.
+    cache
+        .into_iter()
+        .skip(first_match_ix.unwrap_or(0))
+        .filter_map(|cell| cell)
+        .min()
+}
+
 fn calc_mismatch_score(
     left: Option<u32>,
     is_left_continuation: bool,
@@ -67,139 +133,99 @@ fn calc_score(
     }
 }
 
-/// Computes Powierża coefficient of a pattern and a piece of text. For a
-/// detailed explanation, see [lib's docs](crate).
-pub fn powierża_coefficient(pattern: &str, text: &str) -> Option<u32> {
-    let text_len = text.chars().count();
-    let pattern_len = pattern.chars().count();
-
-    // The coefficient is undefined if either of the three is true:
-    // 1. The pattern is longer than the text.
-    // 2. The pattern length is 0.
-    // 3. The text length is 0. (This check is unnecessary but makes the
-    //    code more understandable.)
-    if pattern_len > text_len || pattern_len == 0 || text_len == 0 {
-        return None;
-    }
-
-    // To save memory, only one "row" of cells is stored at any given time.
-    // Note that it contains cells from two rows, because it is filled
-    // sequentially - after the last cell of the previous row was filled,
-    // the algorithm begins filling the next row from the left.
-
-    // The first row is special, because there is nothing in the diagonal
-    // (upper-left) cells. It is padded with `Some(0)` instead of `None` to
-    // prevent premature termination.
-    let mut cache = vec![Some(0); text_len];
-
-    // The score of the cell on the left. It is cached, because otherwise it
-    // would overwrite the diagonal cell. After the left and the diagonal
-    // cell are compared, the diagonal one is overwritten.
-    let mut left = None;
-    let mut is_left_continuation = false;
-
-    // If no match was found in the whole row, the algorithm terminates.
-    let mut does_row_contain_match;
-
-    // Index of the first match in the current row.
-    let mut first_match_ix = 0;
-
-    for pattern_c in pattern.chars() {
-        does_row_contain_match = false;
-        left = None;
-
-        // `Some(0)` set only for x = 0. The result — the pattern can begin at
-        // any place in the string.
-        let mut diagonal = Some(0);
-
-        for (x, text_c) in text.chars().enumerate().skip(first_match_ix) {
-            let is_match = pattern_c == text_c;
-
-            let (current_score, is_continuation) =
-                calc_score(is_match, diagonal, left, is_left_continuation);
-
-            if is_continuation {
-                if !does_row_contain_match {
-                    first_match_ix = x;
-                }
-
-                does_row_contain_match = true;
-            }
-
-            if x > 0 {
-                cache[x - 1] = left;
-            }
-
-            left = current_score;
-            is_left_continuation = is_continuation;
-            diagonal = cache[x];
-        }
-
-        if !does_row_contain_match {
-            return None;
-        }
-    }
-
-    // Fill the score of the right-most cell in the last row.
-    cache[text_len - 1] = left;
-
-    // The lowest score in the last row is the coefficient.
-    cache
-        .into_iter()
-        .skip(first_match_ix)
-        .filter_map(|cell| cell)
-        .min()
-}
 
 #[cfg(test)]
 mod test {
     use super::powierża_coefficient as powierża;
 
     #[test]
-    fn test_empty() {
-        assert!(powierża("", "").is_none());
+    fn test_coefficient_should_be_undefined_if_pattern_is_empty() {
+        assert!(powierża("", "text is not empty").is_none());
     }
 
     #[test]
-    fn test_pattern_longer_than_text() {
-        assert!(powierża("abc", "").is_none());
+    fn test_coefficient_should_be_undefined_if_text_is_empty() {
+        assert!(powierża("pattern is not empty", "").is_none());
     }
 
     #[test]
-    fn test_no_match() {
+    fn test_coefficient_should_be_undefined_if_text_is_longer_than_tet() {
+        assert!(powierża(
+            "pattern is not empty______________________________________",
+            "text is not empty but it is shorter than the pattern"
+        ).is_none());
+    }
+
+    #[test]
+    fn test_coefficient_should_be_undefined_if_pattern_is_not_present_in_text() {
         assert!(powierża("abc", "xyz").is_none());
     }
 
     #[test]
-    fn test_match_position() {
-        assert_eq!(powierża("abc", "abc").unwrap(), 0);
-        assert_eq!(powierża("abc", "abc__").unwrap(), 0);
-        assert_eq!(powierża("abc", "_abc_").unwrap(), 0);
-        assert_eq!(powierża("abc", "__abc").unwrap(), 0);
+    fn test_coefficient_value_with_progressing_number_of_gaps() {
+        let pattern = "xddd";
+        assert_eq!(powierża(pattern, "xddd").unwrap(), 0);
+        assert_eq!(powierża(pattern, "xdd_d").unwrap(), 1);
+        assert_eq!(powierża(pattern, "xd_d_d").unwrap(), 2);
+        assert_eq!(powierża(pattern, "x_d_d_d").unwrap(), 3);
     }
 
     #[test]
-    fn test_abcd() {
-        let pattern = "abcd";
+    fn test_coefficient_value_should_not_change_when_part_with_higher_value_is_added() {
+        let pattern = "xddd";
 
-        assert_eq!(powierża(pattern, "abc_a_b_c_d").unwrap(), 1);
-        assert_eq!(powierża(pattern, "a_b_c_d_bcd").unwrap(), 1);
+        // The prefix `x_d_d_d` should not influence the coefficient
+        // because matching it would result in higher (worse) score.
+
+        assert_eq!(powierża(pattern, "x_d_d_d___xddd").unwrap(), 0);
+        assert_eq!(powierża(pattern, "x_d_d_d___xdd_d").unwrap(), 1);
+        assert_eq!(powierża(pattern, "x_d_d_d___xd_d_d").unwrap(), 2);
+        assert_eq!(powierża(pattern, "x_d_d_d___x_d_d_d").unwrap(), 3);
     }
 
     #[test]
-    fn test_abcjkl() {
-        let pattern = "abcjkl";
+    fn test_coefficient_value_should_not_change_when_gap_length_changes() {
+        let pattern = "xddd";
 
-        assert_eq!(powierża(pattern, "abcjkl").unwrap(), 0);
-        assert_eq!(powierża(pattern, "abc_jkl").unwrap(), 1);
-        assert_eq!(powierża(pattern, "a_bcjkl").unwrap(), 1);
-        assert_eq!(powierża(pattern, "abc_jk_abcj_l").unwrap(), 2);
-        assert_eq!(powierża(pattern, "a_b_c_jkl_ab_c_jkl").unwrap(), 2);
-        assert_eq!(powierża(pattern, "a_b_c_abc_j_k_l_jkl").unwrap(), 1);
+        assert_eq!(powierża(pattern, "x_d_d_d").unwrap(), 3);
+        assert_eq!(powierża(pattern, "x_d_d__d").unwrap(), 3);
+        assert_eq!(powierża(pattern, "x_d__d__d").unwrap(), 3);
+        assert_eq!(powierża(pattern, "x__d__d__d").unwrap(), 3);
+        assert_eq!(powierża(pattern, "x__d__d__d").unwrap(), 3);
+        assert_eq!(powierża(pattern, "x__d__d__d").unwrap(), 3);
     }
 
     #[test]
-    fn test_real_folders() {
+    fn test_coefficient_value_should_not_change_when_gap_is_moved() {
+        let pattern = "xddd";
+
+        assert_eq!(powierża(pattern, "xdd_d").unwrap(), 1);
+        assert_eq!(powierża(pattern, "xd_dd").unwrap(), 1);
+        assert_eq!(powierża(pattern, "x_ddd").unwrap(), 1);
+    }
+
+    #[test]
+    fn test_coefficient_value_should_not_change_when_prefix_is_added() {
+        let pattern = "xddd";
+
+        assert_eq!(powierża(pattern, "xddd_").unwrap(), 0);
+        assert_eq!(powierża(pattern, "xdd_d_").unwrap(), 1);
+        assert_eq!(powierża(pattern, "xd_d_d_").unwrap(), 2);
+        assert_eq!(powierża(pattern, "x_d_d_d_").unwrap(), 3);
+    }
+
+    #[test]
+    fn test_coefficient_value_should_not_change_when_suffix_is_added() {
+        let pattern = "xddd";
+
+        assert_eq!(powierża(pattern, "_xddd").unwrap(), 0);
+        assert_eq!(powierża(pattern, "_xdd_d").unwrap(), 1);
+        assert_eq!(powierża(pattern, "_xd_d_d").unwrap(), 2);
+        assert_eq!(powierża(pattern, "_x_d_d_d").unwrap(), 3);
+    }
+
+    #[test]
+    fn test_coefficient_value_with_real_folder_names() {
         let pattern = "de";
 
         assert_eq!(powierża(pattern, ".docker").unwrap(), 1);
